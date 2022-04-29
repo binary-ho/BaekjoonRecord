281715107c33bdf5177e8ed90f96c509ff492e83
binary_ho <dfghcvb11@naver.com>
2022-04-29 22:27:20 +0900

    [Gold V] Title: 두 용액, Time: 68 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int N, left, right, solution1, solution2;
+    cin >> N;
+    left = solution1 = 0;
+    right = solution2 = N-1;
+    vector<int> vec(N, 0);
+    for(int i = 0; i < N; i++)  cin >> vec[i];
+    sort(vec.begin(), vec.end());
+    int property = abs(vec[left] + vec[right]);
+    while(left < right) {
+        if(property > abs(vec[left] + vec[right])) {
+            property = abs(vec[left] + vec[right]);
+            solution1 = left;
+            solution2 = right;
+        }
+        if(abs(vec[left + 1] + vec[right]) < abs(vec[left] + vec[right - 1])) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    cout << vec[solution1] << " " << vec[solution2];
+}
