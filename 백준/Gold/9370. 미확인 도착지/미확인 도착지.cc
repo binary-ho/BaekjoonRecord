100b5e38e9d1c5a7f4056e86d756f60f0c64cb08
binary_ho <dfghcvb11@naver.com>
2022-05-05 19:29:55 +0900

    [Gold II] Title: 미확인 도착지, Time: 36 ms, Memory: 4408 KB -BaekjoonHub

--- 백준/Gold/9370. 미확인 도착지/미확인 도착지.cc
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.cc
@@ -1,38 +1,23 @@
 #include <iostream>
+#include <string>
 #include <vector>
+#include <set>
 #include <queue>
 #include <algorithm>
 
 #define INF 50000001
 
 using namespace std;
-int n, m, t, s, g, h, src, dest, cost, T, node_now, dist_now, next_node;
-vector<pair<int, int>> vec[2001];
-priority_queue<pair<int, int>> que;
-int dist[3][2001];
-void dijkstra(int start, int dist_num) {
-    dist[dist_num][start] = 0;
-    que.push({0, start});
-    while(!que.empty()) {
-        node_now = que.top().second;
-        dist_now = -que.top().first;
-        que.pop();
-        if(dist[dist_num][node_now] < dist_now) continue;
-        for(int i = 0; i < vec[node_now].size(); i++) {
-            next_node = vec[node_now][i].first;
-            cost = vec[node_now][i].second;
-            if(dist[dist_num][next_node] <= dist_now + cost) continue;
-            dist[dist_num][next_node] = dist_now + cost;
-            que.push({-dist[dist_num][next_node], next_node});
-        }
-    }
-}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
-    vector<int> ans;
 
+    priority_queue<pair<int, int>> que;
+    int dist[2001], dist2[2001], dist3[2001];
+    vector<pair<int, int>> vec[2001];
+    vector<int> ans;
+    int n, m, t, s, g, h, src, dest, cost, T, node_now, dist_now, next_node, must_pass;
     cin >> T;
     while(T--) {
 
@@ -41,7 +26,7 @@ int main() {
 
         for(int i = 0; i <= n; i++) {
             vec[i] = {};
-            dist[0][i] = dist[1][i] = dist[2][i] = INF;
+            dist[i] = dist2[i] = dist3[i] = INF;
         }
         for(int i = 0; i < m; i++) {
             cin >> src >> dest >> cost;
@@ -53,16 +38,59 @@ int main() {
             cin >> dest_vec[i];
         }
 
-        dijkstra(s, 0);
-        dijkstra(g, 1);
-        dijkstra(h, 2);
-
-
-        ans.clear();
-        int g_to_h = dist[1][h];
-        for(int node : dest_vec) { 
-            if(dist[0][g] + g_to_h + dist[2][node] == dist[0][node]) ans.emplace_back(node);
-            else if(dist[0][h] + g_to_h + dist[1][node] == dist[0][node]) ans.emplace_back(node);
+        dist[s] = 0;
+        que.push({0, s});
+        while(!que.empty()) {
+            node_now = que.top().second;
+            dist_now = -que.top().first;
+            que.pop();
+            if(dist[node_now] < dist_now) continue;
+            for(int i = 0; i < vec[node_now].size(); i++) {
+                next_node = vec[node_now][i].first;
+                cost = vec[node_now][i].second;
+                if(dist[next_node] <= dist_now + cost) continue;
+                dist[next_node] = dist_now + cost;
+                que.push({-dist[next_node], next_node});
+            }
+        }
+        must_pass = g;
+        dist2[must_pass] = 0;
+        que.push({0, must_pass});
+        while(!que.empty()) {
+            node_now = que.top().second;
+            dist_now = -que.top().first;
+            que.pop();
+            if(dist2[node_now] < dist_now) continue;
+            for(int i = 0; i < vec[node_now].size(); i++) {
+                next_node = vec[node_now][i].first;
+                cost = vec[node_now][i].second;
+                if(dist2[next_node] <= dist_now + cost) continue;
+                dist2[next_node] = dist_now + cost;
+                que.push({-dist2[next_node], next_node});
+            }
+        }
+        must_pass = h;
+        dist3[must_pass] = 0;
+        que.push({0, must_pass});
+        while(!que.empty()) {
+            node_now = que.top().second;
+            dist_now = -que.top().first;
+            que.pop();
+            if(dist3[node_now] < dist_now) continue;
+            for(int i = 0; i < vec[node_now].size(); i++) {
+                next_node = vec[node_now][i].first;
+                cost = vec[node_now][i].second;
+                if(dist3[next_node] <= dist_now + cost) continue;
+                dist3[next_node] = dist_now + cost;
+                que.push({-dist3[next_node], next_node});
+            }
+        }
+        // dist2[후보] + dist[must_pass] != dist[후보] 면 탈릭
+        int g_to_h = dist2[h];
+        ans = {};
+        for(int node : dest_vec) {
+            if(dist[h] + g_to_h +  dist2[node] == dist[node]) ans.emplace_back(node);
+            else if(dist[g] + g_to_h +  dist3[node] == dist[node]) ans.emplace_back(node);
         }
         sort(ans.begin(), ans.end());
         for(int num : ans) {
