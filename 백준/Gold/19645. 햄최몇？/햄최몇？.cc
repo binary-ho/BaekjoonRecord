00edeea0f5b609d22f0d845d38cd2cc224ff6a95
binary_ho <dfghcvb11@naver.com>
2022-05-06 00:07:18 +0900

    [Gold I] Title: 햄최몇?, Time: 204 ms, Memory: 8128 KB -BaekjoonHub

--- 백준/Gold/19645. 햄최몇？/햄최몇？.cc
+++ 백준/Gold/19645. 햄최몇？/햄최몇？.cc
@@ -2,7 +2,7 @@
 #include <vector>
 
 using namespace std;
-int dp[2501][2501];
+bool dp[2501][2501];
 int N, sum;
 int main() {
     ios_base::sync_with_stdio(false);
@@ -15,30 +15,28 @@ int main() {
         cin >> hamburgers[i];
         sum += hamburgers[i];
     }
-    for(int j = 0; j <= sum; j++) for(int k = 0; k <= sum; k++) dp[j][k] = -1;
     int hamburger;
     for(int i = 0; i < N; i++) {
         hamburger = hamburgers[i];
         if(i == 0) {
-            dp[0][0] = hamburger;
-            dp[0][hamburger] = 0;
-            dp[hamburger][0] = 0;
+            dp[0][0] = true;
+            dp[0][hamburger] = true;
+            dp[hamburger][0] = true;
             continue;
         }
         // 이번 꺼를 선1 선2 나 이렇게 3명이 먹을 수 있음
         for(int j = sum; j >= 0; j--) {
             for(int k = sum; k >= 0; k--) {
-                if(dp[j][k] == -1) continue;
-                dp[j + hamburger][k] = dp[j + hamburger][k] == -1 ? dp[j][k] : min(dp[j + hamburger][k] ,dp[j][k]);
-                dp[j][k+hamburger] = dp[j][k+hamburger]== -1 ? dp[j][k] : min(dp[j][k+hamburger],dp[j][k]);
-                dp[j][k] = dp[j][k] + hamburger;
+                if(!dp[j][k]) continue;
+                dp[j + hamburger][k] = true;
+                dp[j][k+hamburger] = true;
             }
         }
     }
     int ans = 0;
     for(int j = 0; j <= sum; j++) {
         for(int k = 0; k <= sum; k++) {
-            if(dp[j][k] == -1) continue;
+            if(!dp[j][k]) continue;
             if(j < sum - j - k || k < sum - j - k || ans >= sum - j - k) continue;
             ans = sum - j - k;
         }
