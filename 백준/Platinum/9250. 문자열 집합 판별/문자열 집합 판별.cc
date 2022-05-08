ea2cf40d60ff580eec58370b9c9d61d14d98460a
binary_ho <dfghcvb11@naver.com>
2022-05-08 19:29:11 +0900

    [Platinum II] Title: 문자열 집합 판별, Time: 136 ms, Memory: 22908 KB -BaekjoonHub

--- 백준/Platinum/9250. 문자열 집합 판별/문자열 집합 판별.cc
+++ 백준/Platinum/9250. 문자열 집합 판별/문자열 집합 판별.cc
@@ -12,22 +12,7 @@ using namespace std;
 int toNum(char c) { return c - 'a'; }
 char str_arr[10001];
 string str;
-vector<int> str_vec;
-//string str;
-set<char> char_set;
 
-vector<char> arr_copy (const char* arr) {
-    vector<char> vec;
-    int idx = 0;
-    while(*arr) vec.emplace_back(*arr);
-    return vec;
-}
-
-void string_copy (vector<int> &vec, string input) {
-    vec.clear();
-    for(int i = 0; i < input.size(); i++)vec.emplace_back((int)input[i]);
-}
-int keyCnt;
 struct TrieNode
 {
     bool terminal = false;
@@ -52,38 +37,6 @@ struct TrieNode
             children[next]->insert(key + 1);
         }
     }
-
-    TrieNode* find(const char* key) {
-        if (*key == 0) return this;
-        int next = toNum(*key);
-        if (children[next] == nullptr) return nullptr;
-        return children[next]->find(key + 1);
-    }
-    bool find(int idx) {
-        if(terminal) return true;
-        if(!str[idx]) return false;
-        int next = toNum(str[idx]); // 개행문자 체크 필요
-        if(children[next] == nullptr) return false; // 아예 가망도 없다
-        return children[next]->find(idx + 1);
-    }
-    int find(int left, int right) {
-        if(left == right) {
-            if(terminal) return 1; // 완성된 문자열이 존재
-            else return 0;  // 존재 X
-        }
-        int next = toNum(str_arr[left]);
-        if(children[next] == nullptr) return -1; // 아예 가망도 없다
-        return children[next]->find(left + 1, right);
-    }
-    int find(const char* key, int left, int right) {
-        if(left == right) {
-            if(terminal) return 1; // 완성된 문자열이 존재
-            else return 0;  // 존재 X
-        }
-        int next = toNum(key[left]);
-        if(children[next] == nullptr) return -1; // 아예 가망도 없다
-        return children[next]->find(key, left + 1, right);
-    }
 };
 
 void trieBfs(TrieNode *root) {
@@ -96,18 +49,13 @@ void trieBfs(TrieNode *root) {
         que.pop();
         for(int ch = 0; ch < C_SIZE; ch++) {
             TrieNode *next_node = node_now->children[ch];
-            if(next_node == nullptr) continue;    // 다음 철자 없으면 pass
+            if(next_node == nullptr) continue;
             if(node_now == root) next_node->fail = root;
             else {
-                // 부모의 실패 연결을 추적하며 실패 연결을 뒤져가며,
-                // 접미사 노드중 현재 노드의 문자열에 다음 철자를 붙인 문자열의 접미사를 가지고 있는 노드를 찾습니다.
                 TrieNode *ptr = node_now->fail;
                 while(ptr != root && ptr->children[ch] == nullptr) ptr = ptr->fail;
-
-                // 탐색하는 동안 발견 했을 경우 그 노드의 포인터를 가져옵니다.
                 if(ptr->children[ch]) ptr = ptr->children[ch];
                 next_node->fail = ptr;
-                // 발견 못 했을 경우엔 자연스럽게 root node를 가르키게 되겠습니다.
             }
             if(next_node->fail->terminal) next_node->terminal = true;
             que.push(next_node);
@@ -122,7 +70,6 @@ bool ahoCorasick(const string &str, TrieNode *root) {
         int ch = toNum(str[i]);
         while(state != root && state->children[ch] == nullptr) {
             state = state->fail;
-            if(state->terminal) return true;
         }
         if(state->children[ch]) state = state->children[ch];
         if(state->terminal) return true;
@@ -139,7 +86,6 @@ int main() {
     int N, Q;
     cin >> N;
     TrieNode root;
-    keyCnt = 0;
     register int i = 0;
     for(i = 0; i < N; i++) {
         cin >> str_arr;
