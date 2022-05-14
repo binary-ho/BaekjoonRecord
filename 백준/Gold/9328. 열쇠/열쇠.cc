870141edfba35fc99be1395ae04e38187835be5b
binary_ho <dfghcvb11@naver.com>
2022-05-15 05:38:32 +0900

    [Gold I] Title: 열쇠, Time: 24 ms, Memory: 2336 KB -BaekjoonHub

--- 백준/Gold/9328. 열쇠/열쇠.cc
+++ 백준/Gold/9328. 열쇠/열쇠.cc
@@ -10,12 +10,11 @@ int dy[] = {0,0,1,-1};
 int dx[] = {1,-1,0,0};
 bool check[101][101][30];
 bool keyCheck[27];
-bool docCheck[101][101];
 char map[101][101];
 
 void bfs() {
-    int y_now, x_now, new_y, new_x, phase_now, phase = 0;
-    queue<pair<pair<int,int>, int>> que;
+    int y_now, x_now, new_y, new_x, phase = 0;
+    queue<pair<int,int>> que;
     // 동적으로 하기 위해 while로 바꿔야 함
     while(phase <= key_cnt + 1) {
         for(pair<int, int> window : windows) {
@@ -27,21 +26,21 @@ void bfs() {
             } else if(map[window.first][window.second] == '$') doc_cnt++;
             map[window.first][window.second] = '.';
             check[window.first][window.second][phase] = true;
-            que.push({{window.first, window.second}, phase});
+            que.push({window.first, window.second});
         }
         while(!que.empty()) {
-            y_now = que.front().first.first;
-            x_now = que.front().first.second;
-            phase_now = que.front().second;
+            y_now = que.front().first;
+            x_now = que.front().second;
+            
             que.pop();
             for(int i = 0; i < 4; i++) {
                 new_y = y_now + dy[i]; new_x = x_now + dx[i];
                 if(0 > new_y || new_y >= h || 0 > new_x || new_x >= w) continue;
-                if(map[new_y][new_x] == '*' || check[new_y][new_x][phase_now]) continue;
+                if(map[new_y][new_x] == '*' || check[new_y][new_x][phase]) continue;
                 if('A' <= map[new_y][new_x] && map[new_y][new_x] <= 'Z' && !keyCheck[map[new_y][new_x] - 'A']) continue;
 
                 // 이제 가능한 부분
-                check[new_y][new_x][phase_now] = true;
+                check[new_y][new_x][phase] = true;
                 if('A' <= map[new_y][new_x] && map[new_y][new_x] <= 'Z') map[new_y][new_x] = '.';
                 else if('a' <= map[new_y][new_x] && map[new_y][new_x] <= 'z') {
                     if(!keyCheck[map[new_y][new_x] - 'a'])key_cnt++;
@@ -50,7 +49,7 @@ void bfs() {
                 } else if(map[new_y][new_x] == '$') doc_cnt++;
 
                 map[new_y][new_x] = '.';
-                que.push({{new_y, new_x}, phase_now});
+                que.push({new_y, new_x});
             }
         }
         phase++;
