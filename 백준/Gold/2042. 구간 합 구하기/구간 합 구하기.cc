82fd7912f7387e6e4f77941b1f38678857ccf7c9
binary_ho <dfghcvb11@naver.com>
2022-07-13 02:56:16 +0900

    [Gold I] Title: 구간 합 구하기, Time: 356 ms, Memory: 30124 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -3,24 +3,36 @@
 #include <algorithm>
 using namespace std;
 long long N, M, K, arr[1000001], tree[1<<21];
-long long remember_idx[1000001];
+int remember_idx[1000001];
 
-long long make_tree(long long node_now, long long start, long long end) {
+long long make_tree(int node_now, int start, int end) {
     if(start == end) {
         remember_idx[start] = node_now;
         return tree[node_now] = arr[start];
     }
-    long long mid = ((start + end) >> 1);
+    int mid = ((start + end) >> 1);
     return tree[node_now] = make_tree(node_now*2, start, mid) + make_tree(node_now*2+1, mid+1, end);
 }
 
+void update_tree(int node_now, int start, int end, int target_node, int new_num) {
+    if(start==end) {
+        tree[target_node] = new_num;
+        return;
+    }
+    int mid =((start+end)>>1);
+    if(target_node <= mid) update_tree(2*node_now, start, mid, target_node, new_num);
+    else update_tree(2*node_now+1, mid+1,end,target_node, new_num);
+    tree[node_now] = tree[2*node_now] + tree[2*node_now+1];
+    return;
+}
 
-long long get_sum(long long node_now, long long start, long long end, long long ans_start, long long ans_end) {
+
+long long get_sum(int node_now, int start, int end, int ans_start, int ans_end) {
     if(ans_end < start || end < ans_start) return 0;
     if(ans_start <= start && end <= ans_end) {
         return tree[node_now];
     }
-    long long mid = ((start+end)>>1);
+    int mid = ((start+end)>>1);
     return get_sum(node_now*2, start, mid, ans_start, ans_end) + get_sum(node_now*2+1, mid+1, end, ans_start, ans_end);
 }
 
@@ -31,6 +43,8 @@ int main() {
     }
     // 여기서 제작
     make_tree(1,1,N);
+
+
     long long a, b, c;
     for(int i = 0; i < M + K; i++) {
         scanf("%lld %lld %lld", &a, &b, &c);
@@ -39,7 +53,7 @@ int main() {
             long long X = c - arr[b];
             arr[b] = c;
             // arr[remember_idx[b]] = c;
-            long long idx = remember_idx[b];
+            int idx = remember_idx[b];
             while(idx >= 1) {
                 tree[idx] += X;
                 idx >>= 1;
